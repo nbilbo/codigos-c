44dff2dfe58ba47f119b741a9f60c79bb0e48c37
nbilbo <ddtankx68@gmail.com>
2021-01-18 01:57:45 -0300

    versao 0.1

--- estudos/jogodavelha.c
+++ estudos/jogodavelha.c
@@ -25,37 +25,38 @@ char checar_ganhador(char *matriz, int linhas, int colunas);
 void turno_jogador(char *matriz, int linhas, int colunas);
 void turno_maquina(char *matriz, int linhas, int colunas);
 
-// principal
-int main(void){
-    // testes
-    /*
-	char matriz[LINHAS][COLUNAS] = {
-		'o', 'o', 'o',
-		'o', 'o', 'x',
-		'o', 'o', 'o'
-	};
-    
-	display_matriz(&matriz[0][0], LINHAS, COLUNAS);
-    // printf("%c \n", checar_linha(&matriz[1][0], COLUNAS));
-	// printf("%c \n", checar_coluna(&matriz[0][2], LINHAS, COLUNAS));
-    // printf("%c \n", checar_diagonal(&matriz[0][0], LINHAS, COLUNAS));
-    //printf("%c \n", checar_diagonal_inverso(&matriz[0][COLUNAS-1], LINHAS, COLUNAS));
-    */
-    
+int main(void){   
     // vars
-    char matriz[LINHAS][COLUNAS] = {
-        'x', 'x', 'o',
-        'o', ' ', ' ',
-        'x', 'x', 'o'
-    };
-    char vencedor = '\0';
+    char matriz[LINHAS][COLUNAS];
+    char jogadores[] = {'x', 'o'}, atual;
+    char ganhador = '\0';
     int restantes = LINHAS*COLUNAS;
     
-    turno_maquina(&matriz[0][0], LINHAS, COLUNAS);
+    // game loop
+    reset_matriz(&matriz[0][0], LINHAS, COLUNAS);
+    atual = jogadores[1];
+
+    while(!ganhador && restantes){
+        printf("\nJogadas restantes: %d\n", restantes);
+        display_matriz(&matriz[0][0], LINHAS, COLUNAS);
+
+        if(atual == 'x') turno_jogador(&matriz[0][0], LINHAS, COLUNAS);
+        else turno_maquina(&matriz[0][0], LINHAS, COLUNAS);
+
+        ganhador = checar_ganhador(&matriz[0][0], LINHAS, COLUNAS);
+        if(ganhador == ' ') ganhador = '\0';
+
+        atual = atual == 'x' ? 'o' : 'x';
+        restantes--;
+    }
+
+    printf("\nFim\n");;
+    printf("\nJogadas restantes: %d\n", restantes);
     display_matriz(&matriz[0][0], LINHAS, COLUNAS);
-    printf("%c \n", checar_ganhador(&matriz[0][0], LINHAS, COLUNAS));
+    if(ganhador) printf("\nVencedor: %c\n", ganhador);
+    else printf("\nEmpate\n");
     
-	return 0;
+    return 0;
 }
 
 
@@ -64,14 +65,11 @@ Definir espaços vazios em cada elemento da matriz.
 
 parametros
 ----------
-matriz : ponteiro do tipo char
-	um ponteiro para o primeiro elemento da matriz.
+matriz : um ponteiro para o primeiro elemento da matriz.
 
-linhas : int
-    quantidade de linhas da matriz.
+linhas : quantidade de linhas da matriz.
 
-colunas : int
-    quantidade de colunas na matriz.
+colunas : quantidade de colunas na matriz.
 */
 void reset_matriz(char *matriz, int linhas, int colunas){
     for(register int i=0; i<linhas; i++)
@@ -85,22 +83,19 @@ Imprime os elementos da matriz.
 
 parametros
 ----------
-matriz : ponteiro do tipo char
-	um ponteiro para o primeiro elemento da matriz.
+matriz : um ponteiro para o primeiro elemento da matriz.
 
-linhas : int
-    quantidade de linhas da matriz.
+linhas : iquantidade de linhas da matriz.
 
-colunas : int
-    quantidade de colunas em cada linha da matriz.
+colunas : quantidade de colunas em cada linha da matriz.
 */
 void display_matriz(char *matriz, int linhas, int colunas){
-	for(register int i=0; i<linhas; i++){
-		for(register int j=0; j<colunas; j++){
+    for(register int i=0; i<linhas; i++){
+        for(register int j=0; j<colunas; j++){
             printf("[%c] ", *(matriz+(i*colunas)+j));
-		}
+        }
         printf("\n");
-	}	
+    }   
 }
 
 
@@ -109,11 +104,9 @@ Verificar se os elementos da linha são iguais.
 
 parametros
 ----------
-matriz : ponteiro do tipo char.
-    ponteiro para o primeiro elemento da linha.
+matriz : ponteiro para o primeiro elemento da linha.
 
-colunas : int
-    quantidade de colunas na matriz.
+colunas : quantidade de colunas na matriz.
 
 retorno
 -------
@@ -137,14 +130,11 @@ Verificar se os elementos da coluna são iguais.
 
 parametros
 ----------
-matriz : ponteiro do tipo char.
-    ponteiro para o primeiro elemento da coluna.
+matriz : ponteiro para o primeiro elemento da coluna.
 
-linhas : int
-    quantidade de linhas da matriz.
+linhas : quantidade de linhas da matriz.
 
-colunas : int
-    quantidade de colunas na matriz.
+colunas : quantidade de colunas na matriz.
 
 retorno
 -------
@@ -168,14 +158,11 @@ Verificar os elementos na diagonal da matriz.
 
 parametros
 ----------
-matriz : ponteiro do tipo char
-    ponteiro do primeiro elemento da matriz.
+matriz : ponteiro do primeiro elemento da matriz.
 
-linhas : int
-    quantidade de linhas na matriz.
+linhas : quantidade de linhas na matriz.
 
-colunas : int
-    quantidade de colunas na matriz.
+colunas : quantidade de colunas na matriz.
 
 retorno
 -------
@@ -199,14 +186,11 @@ Verificar os elementos na diagonal inversa da matriz.
 
 parametros
 ----------
-matriz : ponteiro do tipo char
-    ponteiro do ultimo elemento da primeira linhda da matriz.
+matriz : ponteiro do ultimo elemento da primeira linhda da matriz.
 
-linhas : int
-    quantidade de linhas na matriz.
+linhas : quantidade de linhas na matriz.
 
-colunas : int
-    quantidade de colunas em cada linha.
+colunas : quantidade de colunas em cada linha.
 
 retorno
 -------
@@ -230,14 +214,11 @@ Realizar a jogada do jogador.
 
 parametros
 ----------
-matriz : ponteiro do tipo char.
-    ponteiro para o primeiro elemento da matriz.
+matriz : ponteiro para o primeiro elemento da matriz.
 
-linhas : int
-    quantidade de linhas da matriz.
+linhas : quantidade de linhas da matriz.
 
-colunas : int
-    quantidade de colunas da matriz.
+colunas : quantidade de colunas da matriz.
 */
 void turno_jogador(char *matriz, int linhas, int colunas){
     int linha, coluna;
@@ -264,14 +245,11 @@ Checar todas as possibilidades por um vencedor na matriz.
 
 parametros
 ----------
-matriz : ponteiro do tipo char.
-    ponteiro para o primeiro elemento da matriz.
+matriz : ponteiro para o primeiro elemento da matriz.
 
-linhas : int
-    quantidade de linhas na matriz.
+linhas : quantidade de linhas na matriz.
 
-colunas : int
-    quantidade d ecolunas na matriz.
+colunas : quantidade d ecolunas na matriz.
 
 retorno
 -------
@@ -309,7 +287,8 @@ char checar_ganhador(char *matriz, int linhas, int colunas){
 
 
 /*
-Percorrer por toda matriz procurando um elemento disponivel.
+Percorrer por toda matriz procurando um elemento disponivel p/ a maquina
+realizar a jogada.
 
 parametros
 ----------
@@ -334,5 +313,4 @@ void turno_maquina(char *matriz, int linhas, int colunas){
     
     *elemento = 'o';
 }
-
 
